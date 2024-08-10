6183da18cc0eec05d09d9f3dd90e3d50f1ac61cd
hakenshi <fkdias025@gmail.com>
2024-08-10 11:16:00 -0300

    update/animação da side-bar

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -14,7 +14,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-
         User::create([
             'email' => 'adm@gmail.com',
             'name' => 'adm',
--- laravel/public/js/dropdown.js
+++ laravel/public/js/dropdown.js
@@ -1,8 +1,23 @@
+const hideElment = (element, _class) => {
+    if(element.hasClass(_class) ){
+        element.removeClass(_class)
+    }else{
+        element.addClass(_class)
+    }
+}
+
 $("button[class='padding']").on("click", ()=>{
     const dropdown = $("#dp-profile")
-    if(dropdown.hasClass('dropProfile') ){
-        dropdown.removeClass('dropProfile')
-    }else{
-        dropdown.addClass('dropProfile')
+
+    hideElment(dropdown, 'dropProfile')
+})
+
+$("#nav-button").on('click', () => {
+    const aside = $("aside")
+    if (!aside.hasClass('hidden')){
+        aside.addClass('hidden').removeClass('show')
+    }
+    else{
+        aside.addClass('show').removeClass('hidden')
     }
 })
